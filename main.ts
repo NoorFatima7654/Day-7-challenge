a261eb3eff67bd8fa5505aa8cb8446fab4db0526
Noor Fatima <160162835+NoorFatima7654@users.noreply.github.com>
2024-03-08 19:49:04 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,17 @@
+//Question no 19
+let guests: string[] = ["Mishal","Arish","Noor","Qurat","Usman"];
+console.log(`I am iniviting ${guests.length} to the dinner`);
+//Question no 20
+let countries = ["Japan", "China", "New zeeland", "Switzerland", "Iceland"];
+console.log("countries,I'd like to visit:", countries);
+
+//Question no 21
+let book: { title:string;
+    author: string;
+    yearPublished: number;} = {
+        title: "American Notes",
+        author: "C.Dickens",
+        yearPublished: 1842
+    };
+    console.log(`Book Info: "${book.title}",by ${book.author}, published in ${book.yearPublished}`);
+
